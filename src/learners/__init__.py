6d7d55fb05e0c534a9c50282523c71a197c79cdc
Vivian Huang <vivh@mit.edu>
2020-07-07 14:06:29 -0400

    Update __init__.py

--- src/learners/__init__.py
+++ src/learners/__init__.py
@@ -5,5 +5,3 @@ from .qtran_learner import QLearner as QTranLearner
 REGISTRY = {}
 
 REGISTRY["q_learner"] = QLearner
-REGISTRY["coma_learner"] = COMALearner
-REGISTRY["qtran_learner"] = QTranLearner
